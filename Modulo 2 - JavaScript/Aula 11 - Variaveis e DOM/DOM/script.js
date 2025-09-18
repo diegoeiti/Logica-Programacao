6d34b8f52e63136d5babade0ed797ac2f3b921fb
diegoeiti <diegoeiti96@gmail.com>
2025-09-18 10:44:35 -0300

    .

--- Modulo 2 - JavaScript/Aula 11 - Variaveis e DOM/DOM/script.js
+++ Modulo 2 - JavaScript/Aula 11 - Variaveis e DOM/DOM/script.js
@@ -2,8 +2,30 @@ document.body.style.backgroundColor = "linear-gradient(to right, blue, red)";
 document.body.style.color = "white";
 
 // window.alert("Seja Bem Vindo!")
-def 
+
 // window.prompt("Login");
 // window.prompt("Senha");
 
 // window.confirm("Site com AIDS");
+
+let paragrafo1 = document.getElementsByTagName("p")[0]
+paragrafo1.innerText = "Usando o TAG NAME"
+paragrafo1.style.background = "linear-gradient(to right, blue, green)";
+
+let paragrafo2 = document.getElementById("caixa2")
+paragrafo2.innerText = "Caixa 2 Versao 2"
+paragrafo2.style.background = "linear-gradient(to right, yellow, pink)";
+paragrafo2.style.color = "black";
+
+let paragrafo3 = document.getElementsByClassName("caixa3")[0];
+paragrafo3.style.background = "linear-gradient(to right, black, yellow)";
+
+// document.getElementByName("nome")[0] para acessar o input
+
+let paragrafo4 = document.querySelector("#paragrafo4")
+paragrafo4.innerText = "Caixa 4 Versao 2"
+paragrafo4.style.background = "linear-gradient(to right, yellow, green)";
+
+let paragrafo5 = document.querySelectorAll(".caixa5")[1]
+paragrafo5.innerText = "Caixa 5 Versao 2"
+paragrafo5.style.background = "linear-gradient(to right, purple, white)";
