f8d6f9a3bd999a6e44fbb83314e2fe4e2a4e9056
diegoeiti <diegoeiti96@gmail.com>
2025-09-25 10:06:35 -0300

    .

---
+++ Modulo 2 - JavaScript/Aula 13 - Estrutura Condicional/condicoes/condicoes.js
@@ -0,0 +1,14 @@
+let nota1 = 10 
+let nota2 = 10
+
+let media = (nota1 + nota2) / 2;
+
+if(media >= 7){
+    console.log("Aprovado");
+} 
+else if (media >= 5){
+    console.log("Exame");
+}
+else {
+    console.log("Reprovado");
+}
